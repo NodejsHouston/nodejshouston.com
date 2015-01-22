53ed0f3e39fd72e1bc0efac64552105fc5e201ac
Alan Lee <juxta52@gmail.com>
2015-01-22 12:29:22 -0600

    maybe the config checked in is not a good idea, there are API keys

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'blog.nodejshouston.com',
         mail: {},
         database: {
             client: 'sqlite3',
